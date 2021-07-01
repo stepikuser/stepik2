ad3c6e9c00e5cbce289a1eb7ba831f922d052ced
nesterenko-marina99 <miss.nesterenko.marina@gmail.com>
2021-07-01 05:21:11 +0400

    fixed bugs

--- test_items.py
+++ test_items.py
@@ -1,7 +1,10 @@
 import pytest
+import time
 from selenium import webdriver
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_basket_link_on_the_main_page(browser):
-        browser.get(link)
-        browser.find_element_by_css_selector("button.btn-add-to-basket")
+	browser.get(link)
+	time.sleep(30)
+	assert browser.find_element_by_css_selector("button.btn-add-to-basket") != None
